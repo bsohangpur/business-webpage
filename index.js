2bd18e2083f047ea83bec8c65d46cad3d5b31d4e
bsohangpur <47111080+bsohangpur@users.noreply.github.com>
2022-06-10 12:42:02 +0530

    update all new testimonial section.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// mobile menu js
+
 let menu = document.querySelector(".mobile-menu-line")
         let menuItem = document.querySelector(".menu-item")
         let menuOpen = false;
@@ -17,3 +19,48 @@ let menu = document.querySelector(".mobile-menu-line")
                 
             }
         })
+
+
+// testimonial js
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay:{
+        delay: 2000
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+});
+
+
+
+mobileCode=()=>{
+    if(mobileView.matches){
+        new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay:{
+        delay: 1500,
+        
+    }
+})
+    }
+    else{
+        new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,})
+    }
+}
+
+
+
+const mobileView = window.matchMedia("(max-width: 768px)");
+
+mobileCode(mobileView);
+
+
+
+mobileView.addEventListener("change", mobileCode);
